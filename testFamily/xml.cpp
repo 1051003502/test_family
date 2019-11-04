3435451240d1a86884d6516d6be13c69b62f2041
1051003502 <1051003502@qq.com>
2019-11-04 10:32:00 +0800

    简化了xml格式判定函数

--- testFamily/xml.cpp
+++ testFamily/xml.cpp
@@ -3,27 +3,9 @@
 #include <QtXml>
 #include "inout.h"
 bool isXml(QString data)
-
 {
-
-    //打开或创建文件
-//    QFile f1("test.xml");
-//    if(!f1.open(QFile::WriteOnly))return false;
-//    QTextStream in(&f1);
-//    in<<data;
-//    f1.close();
-//    QFile file("test.xml"); //相对路径、绝对路径、资源路径都行
-
-//    if(!file.open(QFile::ReadOnly))return false;
     QDomDocument doc;
-//    //if(!doc.setContent(&file))
-    if(!doc.setContent(data))
-    {
-        //file.close();
-        //qDebug()<<"xml错误";
-        return false;
-    }
-    //file.close();
+    if(!doc.setContent(data))return false;
     return true;
 }
 Xml::Xml(QWidget *parent) :
